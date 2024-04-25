1b30c998f745b6dc7b258c8b502608ce3aa4c5e0
Rishi Upadhyay <rupadhyay2674@gmail.com>
2024-04-25 19:05:44 +0530

    fix

--- controllers/leaderboard.controller.js
+++ controllers/leaderboard.controller.js
@@ -3,7 +3,7 @@ import {userSchema} from '../models/user.schema';
 async function getLeaderboard(req,res){
     const leaderboard=await userSchema.find({role:0}).sort({
         points:'desc'
-    }).select("username points")
+    }).select("username currentPoints")
     return res.status(200).send({
         success:true,
         leaderboard
