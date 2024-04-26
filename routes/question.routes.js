e16e9e7a3a2778d3dce3ce42fb1711c6515cb60d
DRedDevil04 <devamdesai@gmail.com>
2024-04-26 16:17:45 +0530

    fix

--- routes/question.routes.js
+++ routes/question.routes.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { createProblem, deleteProblem, getAllProblems, updateProblem } from "../controllers/question.controller.js";
+import { createProblem, deleteProblem, getAllProblems, getProblemById, updateProblem } from "../controllers/question.controller.js";
 import { isAdmin, requireSignIn } from "../middlewares/auth.middleware.js";
 import { validateProblem } from "../middlewares/problem.middleware.js";
 
 const router = express.Router();
 
 router.get("/", getAllProblems);
+router.get("/:id",getProblemById)
 router.post("/",requireSignIn,isAdmin,validateProblem,createProblem);
 router.patch("/:id", requireSignIn, isAdmin, updateProblem);
 router.delete("/:id",requireSignIn,isAdmin,deleteProblem);
