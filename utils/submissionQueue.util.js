58daedf3df3726eddfd55c004a85da5d818f77a7
DRedDevil04 <devamdesai@gmail.com>
2024-06-27 12:30:14 +0530

    minor

--- utils/submissionQueue.util.js
+++ utils/submissionQueue.util.js
@@ -23,7 +23,7 @@ submissionQueue.process(async (job, done) => {
 
     const problem = await questionSchema.findOne({ id: job.data.problem });
     const testCases = await testcaseSchema.find({ question: job.data.problem });
-    let flag = 0;
+    let stat = "";
     await Promise.all(
       testCases.map(async (testcase) => {
         var formData = new FormData();
@@ -32,52 +32,44 @@ submissionQueue.process(async (job, done) => {
         formData.append("files", codeBlob);
         formData.append("input", blob);
         formData.append("output", testcase.output);
-        const resp = await axios.post(
-          "http://judge:3005/upload",
-          formData,
-          {
-            headers: {
-              "Content-Type": "multipart/form-data",
-            },
-          }
-        );
-        console.log(resp)
-        if (resp.data.success && resp.data.opstatus !== "accepted") {
-          flag = 1;
+        const resp = await axios.post("http://judge:3005/upload", formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        });
+        console.log(resp);
+        if (resp.data.success == true && resp.data.opstatus == "accepted") {
+          stat = "Accepted";
+        }
+        if (resp.data.success == true && resp.data.opstatus == "wrong") {
+          stat = "Wrong Answer";
+        }
+        if (resp.data.opstatus == "compile") {
+          stat = "Compilation Error";
+        }
+        if (resp.data.opstatus == "run") {
+          stat = "Runtime Error";
         }
       })
     );
-
-    if (flag == 0) {
-      const submissionScheme = await Submission.findByIdAndUpdate(
-        submission._id,
-        {
-          result: "Accepted",
-        },
-        { new: true }
-      );
-      const correctSubmissions = await Submission.find({
-        problem: job.data.problem,
-        user: job.data.userid,
+    const submissionScheme = await Submission.findByIdAndUpdate(
+      submission._id,
+      {
+        result: stat,
+      },
+      { new: true }
+    );
+    const correctSubmissions = await Submission.find({
+      problem: job.data.problem,
+      user: job.data.userid,
+      result:"Accepted"
+    });
+    if (correctSubmissions.length == 1 && submissionSchema.result=="Accepted") {
+      const user = await userSchema.findByIdAndUpdate(job.data.userid, {
+        $inc: { currentPoints: problem.points },
       });
-      if (correctSubmissions.length == 1) {
-        const user = await userSchema.findByIdAndUpdate(job.data.userid, {
-          $inc: { currentPoints: problem.points },
-        });
-      }
-
-      console.log(submissionScheme);
-    } else {
-      const submissionScheme = await Submission.findByIdAndUpdate(
-        submission._id,
-        {
-          result: "Wrong Answer",
-        },
-        { new: true }
-      );
-
-      console.log(submissionScheme);
     }
+    console.log(submissionScheme);
   } catch (error) {
     console.log(error);
   }
